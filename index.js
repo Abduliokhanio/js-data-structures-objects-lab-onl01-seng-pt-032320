567e96b96959b728ad097b45fce42dcef68653ae
Abdul Khan <akhan17@outlook.com>
2020-09-19 17:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromDriverByKey(driver, key){
     return newObj;
   }
 
-  function destructivelyDeleteFromDriverByKey(driver, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
     delete driver[key];
     return driver
   }
